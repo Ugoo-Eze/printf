29b824b8403c44dc05ddb784c0b3f9093c1db7e8
Ugoo <ugonnaezeabara@gmail.com>
2022-07-13 04:11:39 -0700

    updated printf project

---
+++ printf.c
@@ -0,0 +1,14 @@
+#include <stdarg.h>
+#include "main.h"
+
+/**
+ * _printf - prints a string in formatted way
+ * @format: string to print
+ *
+ * Return: number of characters printed
+*/
+
+int _printf(const char *format, ...)
+{
+	return (0);	
+}
